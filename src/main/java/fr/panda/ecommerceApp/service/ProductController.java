09519eb5ced8c047719759fca52d987dcfec2d4a
unknown <objectifmh@hotmail.com>
2020-04-05 11:38:38 +0100

    error.html

--- src/main/java/fr/panda/ecommerceApp/service/ProductController.java
+++ src/main/java/fr/panda/ecommerceApp/service/ProductController.java
@@ -23,6 +23,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -31,6 +32,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
  *
  * @author gtu
  */
+
 @Controller
 public class ProductController {
 
